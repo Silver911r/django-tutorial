d9f34feb89b9d0ea2fc7955deac7ac218c923acc
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-03-10 12:25:42 -0500

    add tests and static files

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -18,7 +19,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return last five questions published."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 # def detail(request, question_id):
@@ -29,6 +32,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 # def results(request, question_id):
